f0cf286e468fa2799324f7b2be249a618eb3ed01
Alp <alp@collectivat.cat>
2021-07-22 12:42:42 +0200

    cors try

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import os
 from app.api.transcribeAPI import transcribe
+from fastapi.middleware.cors import CORSMiddleware
 
 ROOT_PATH = '/' + os.environ.get('PROXY_PREFIX') if os.environ.get('PROXY_PREFIX') else None
 
@@ -9,5 +10,18 @@ app = FastAPI(title="Speech API",
               version="0.1", 
               root_path=ROOT_PATH, 
               redoc_url="/redoc", openapi_url="/openapi.json", docs_url="/docs")
+
+origins = [
+    "http://127.0.0.1",
+    "http://127.0.0.1:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(transcribe, prefix='/transcribe')
