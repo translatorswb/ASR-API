e307cbe91af4e11c6d9368188c5ebe7c59cdb0d7
Ubuntu <faizan@translatorswithoutborders.org>
2022-06-15 11:46:02 +0000

    chooser changed to nlu from spacy

--- app/api/transcribeAPI.py
+++ app/api/transcribeAPI.py
@@ -549,7 +549,7 @@ async def stt(request: Request):
 
     try:
         transcript = speech_to_text(audio_file, lang='eng', mid=mid)
-        lang = chooser_spacy(transcript)
+        lang = chooser_nlu(transcript)
         if not lang == 'eng':
             transcript = speech_to_text(audio_file, lang=lang, mid=mid)
     except Exception as e:
